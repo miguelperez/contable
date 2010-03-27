cca75353bf00e8c45a02c2f84f68f7172b732680
Miguel <miguel.perez@koombea.com>
2010-03-27 00:21:28 -0500

    Separating the way for adding ingredients.

--- app/helpers/shared_helper.rb
+++ app/helpers/shared_helper.rb
@@ -1,14 +1,17 @@
 module SharedHelper
-  #returns a li with a link for a tabbed bar.
+  #Returns a li with a link for a tabbed bar.
   #+link_label+ would be the label for the tab, and also must be the name of the
   #tab that will be shown when the user clicks this link.
   def tab_link(link_label)
     "<li><a href=\"##{link_label.downcase.squeeze(" ").strip.gsub(/ /, '_')}\">#{link_label}</a></li>"
   end
 
-  def tabbed_body(tab_name, &block)
-    concat("<div id=\"#{tab_name.downcase.squeeze(" ").strip.gsub(/ /, '_')}\">", block.binding)
+  #Returns a div which will have a tab body. This receives a block with html that
+  #will be appended into the div.
+  def tabbed_body(tab_name, display = true, &block)
+    class_name = !display ? "class = \"display-none\"" : ""
+    concat("<div #{class_name} id=\"#{tab_name.downcase.squeeze(" ").strip.gsub(/ /, '_')}\">")
     yield
-    concat("</div>", block.binding)
+    concat("</div>")
   end
 end
---
+++ public/javascripts/ingredients.js
@@ -0,0 +1,62 @@
+//Ingredients Helper
+var IngredientsHelper = (function(){
+  var initialize = function(){
+  };
+
+  //creates the add ingredients fields.
+  var createIngredientForm = function(ingredientObject, parentDiv, childElem){
+    //lets assing an id to the containing li, if it does not have id set.
+    childElem.identify();
+    
+    var h4 = new Element('h4').insert(ingredientObject.ingredient.name);
+    var label = new Element('label', {
+      'class':'styled-label'
+    }).insert('Ammount');
+    var hidden = new Element('input', {
+      'type':'hidden',
+      'id':'product_new_formula_attributes__ingredient_id',
+      'name':'product[new_formula_attributes][][ingredient_id]',
+      'value':ingredientObject.ingredient.id
+    });
+    var input = new Element('input', {
+      'type':'text',
+      'class':'styled-input',
+      'id':'product_new_formula_attributes__ingredient_ammount',
+      'name':'product[new_formula_attributes][][ingredient_ammount]'
+    });
+    var roundButton = new Element('div', {
+      'class':'round-button delete',
+      'onclick': '$(this).up(".ingredient").remove(); $("'+childElem.identify()+'").appear();'
+    });
+    //create the container div and insert the products objects
+    var containerDiv = new Element('div', {
+      'class':'ingredient'
+    }).insert(h4).insert(label).insert(hidden).insert(input).insert(roundButton);
+    //adding to parent
+    containerDiv.hide();
+    parentDiv.insert(containerDiv);
+    containerDiv.appear();
+    childElem.fade();
+  }
+
+  //creates the tag
+  var createIngredientTag = function(ingredientObject, parentContainer){
+    var span = new Element('span').insert(ingredientObject.ingredient.name);
+    var json = '{"ingredient":{"name":"'+ingredientObject.ingredient.name+'","id":'+ingredientObject.ingredient.id+'}}';
+    var a = new Element('a', {
+      'href':'#',
+      'class':'tag',
+      'onclick':'IngredientsHelper.createIngredientForm('+ json +', $("ingredients_form"), this.up())'
+    }).insert(span);
+    var li = new Element('li').insert(a);
+
+    parentContainer[0].insert(li);
+  };
+  
+  return{
+    'initialize':initialize,
+    'createIngredientForm': createIngredientForm,
+    'createIngredientTag':createIngredientTag
+  }
+})();
+IngredientsHelper.initialize();
