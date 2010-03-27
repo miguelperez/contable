01e369652459bc65c0264dfcfd5c6b119a6deb14
Miguel <miguel.perez@koombea.com>
2010-03-27 12:03:01 -0500

    Fixing bug when adding ingredients. Now when double clicked only one ingredient gets added.

--- public/javascripts/ingredients.js
+++ public/javascripts/ingredients.js
@@ -4,11 +4,21 @@ var IngredientsHelper = (function(){
   };
 
   //creates the add ingredients fields.
+  //ingredient_object is a json of the ingredient that will be added, it has the ingredient information.
+  //parent_div is the object which will hold the fields for adding the new ingredient.
+  //child_elem is the object which trigger this function to be called, must be hidden after adding the ingredient.
   //label_title is the title that will be added to the ingredient field.
   //hash_name will be the name used for generating the id and name of the created input text.
   var createIngredientForm = function(ingredientObject, parentDiv, childElem, label_title, hash_name){
     //lets assing an id to the containing li, if it does not have id set.
     childElem.identify();
+
+    //check if childElem is hidden if its, then we shall not add the ingredient form
+    if(childElem.getStyle('display') == 'none' || childElem.getStyle('visibility') == 'hidden'){
+      return;
+    }else{
+      childElem.hide();
+    }
     
     var h4 = new Element('h4').insert(ingredientObject.ingredient.name);
     var label = new Element('label', {
@@ -38,10 +48,11 @@ var IngredientsHelper = (function(){
     containerDiv.hide();
     parentDiv.insert(containerDiv);
     containerDiv.show();
-    childElem.hide();
   }
 
-  //creates the tag
+  //creates the ingredient tag
+  //ingredient_object is a json of the ingredient that will be added, it has the ingredient information.
+  //parentContainer is the object which will hold the tag for adding the new ingredient.
   var createIngredientTag = function(ingredientObject, parentContainer){
     var span = new Element('span').insert(ingredientObject.ingredient.name);
     var json = '{"ingredient":{"name":"'+ingredientObject.ingredient.name+'","id":'+ingredientObject.ingredient.id+'}}';
